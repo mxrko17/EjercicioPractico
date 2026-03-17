796d94ed2f7c80d62e9fd28661fbe44f87e2aa7c
mxrko17 <marco.basualto2501@alumnos.ubiobio.cl>
2026-03-17 12:41:17 -0300

    actu

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        Libro libro = new Libro("Victor", "Ayuda", 111);
-        Libro libro1 = new Libro("Vichoneta", "SeMeFueLaRapidos", 10);
+        System.out.println("Cuantos libros hay?");
+        int cantLibro = sc.nextInt();
 
-        libro.mostrarInfo();
-        libro1.mostrarInfo();
+        Libro libros[] = new Libro[cantLibro];
+
+        for (int i=0; i<cantLibro; i++){
+            System.out.println("Ingrese el nombre del autor: ");
+            String nombreAutor = sc.next();
+
+            System.out.println("Ingrese el titulo del libro: ");
+            String nombreLibro = sc.next();
+
+            System.out.println("Ingrese el nro de paginas: ");
+            int cantidadPaginas = sc.nextInt();
+
+            libros[i] = new Libro(nombreAutor, nombreLibro, cantidadPaginas);
+        }
+
+        for (int i=0; i<cantLibro; i++) {
+            libros[i].mostrarInfo();
+        }
     }
 }
